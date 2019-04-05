9381aae1edf04ad3a2f6d8341a01b421206c13c1
ousmane91 <contact@ousmangueye.fr>
2019-04-05 16:37:22 +0200

    exocorriger

--- Exojs7/script.js
+++ Exojs7/script.js
@@ -1,15 +1,25 @@
 function Number(){
-var shoeSize = document.getElementById('shoeSize').value;
-var yearOfBirth = document.getElementById('yearOfBirth').value;
-//multiplie de la pointure indiqué par 2
-var newshoeSize = shoeSize * 2;
-//ajout de 5 au résultat
-var newAdd = newshoeSize + 5;
-//multiplie le total par 50
-var allMultiply = newAdd * 50;
-//soustrait l'année de naissance par raaport au résultat précédent
-var substractofyear = allMultiply - yearOfBirth;
-//ajout au total 1769
-var result = substractofyear + 1769;
-alert(result);
+  var shoeSize = document.getElementById('shoeSize').value;
+  var yearOfBirth = document.getElementById('yearOfBirth').value;
+  //multiplie de la pointure indiqué par 2
+  // var newshoeSize = shoeSize * 2;
+  // //ajout de 5 au résultat
+  // var newAdd = newshoeSize + 5;
+  // //multiplie le total par 50
+  // var allMultiply = newAdd * 50;
+  // //soustrait l'année de naissance par raaport au résultat précédent
+  // var substractofyear = allMultiply - yearOfBirth;
+  // //ajout au total 1769
+  // var result = substractofyear + 1769;
+  var result;
+  result = shoeSize * 2;
+  result = result + 5;
+  result = result * 50;
+  result = result - yearOfBirth;
+  result = result + 1769;
+  if (isNaN(shoeSize) || isNaN(yearOfBirth)) {
+    alert('Veuillez saisir uniquement un nombre');
+  } else {
+    alert(result);
+  }
 }
